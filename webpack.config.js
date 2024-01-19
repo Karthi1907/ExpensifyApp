9c7e99274a5a058fbc0b928e21dbb7259a3da2fe
Singam <tsk.karthik@gmail.com>
2024-01-19 21:25:17 +0530

    Production Mode vs Development Mode changes - MiniCssExtractPlugin usage

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,18 @@
 const path = require('path');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  mode: "development",
+module.exports = (env) => {
+  console.log('env', env);
+  const isProduction = env.production === true;
+  // const CSSExtract = new MiniCssExtractPlugin('styles.css');
+  // const MiniCssExtractPlugin = new MiniCssExtractPlugin({
+  //   filename: "ext-style.css"
+  // });
+  console.log(isProduction);
+
+  return {
+  // mode: "development",
   entry: './src/app.js',
   // entry: './src/playground/advjavascript_examples.js', //K:\ReactCourseProjects\ExpenseNotifyApp\src\playground\advjavascript_examples.js
   output: {
@@ -15,18 +26,28 @@ module.exports = {
       exclude: /node_modules/
     }, {
       test: /\.s?css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    }]
+      // use: 
+      // include: /src/,
+      // use: MiniCSSExtract.extract({
+      //   use: [
+      //     // 'style-loader',
+      //     'css-loader',
+      //     'sass-loader'
+      //   ]
+      // })
+      use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+    }]  
   },
-    devtool : 'eval-cheap-module-source-map',
+    // devtool : isProduction ? 'source-map' : 'eval-cheap-module-source-map',
+    devtool : isProduction ? 'source-map' : 'inline-source-map',
+    plugins: [
+      new MiniCssExtractPlugin( { filename: "ext-style.css"  })
+    ],
     devServer : {
         static: {
             directory : path.join(__dirname, 'public')
         },
         historyApiFallback : true
     }
+  }
 };
